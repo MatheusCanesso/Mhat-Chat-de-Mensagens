7047af5585796a26dcedf80006aab1e15574d41d
Matheus Canesso <61373018+MatheusCanesso@users.noreply.github.com>
2024-09-03 15:32:39 -0300

    Update Script.js

--- frontend/Js/Script.js
+++ frontend/Js/Script.js
@@ -196,7 +196,7 @@ const handleLogin = (event) => {
   login.style.display = "none";
   chat.style.display = "flex";
 
-  websocket = new WebSocket("ws://localhost:8080");
+  websocket = new WebSocket("https://mhat-chat-de-mensagens.onrender.com");
   websocket.onmessage = processMessage;
 };
 
